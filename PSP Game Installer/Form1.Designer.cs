6c2c12b746d0e9aa84a68dc344d9ecbea1c8e7c5
Kitki30 Games <95166343+kitki30@users.noreply.github.com>
2023-06-29 19:35:17 +0200

    First Version

---
+++ PSP Game Installer/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace PSP_Game_Installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(22, 242);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(79, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 226);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select PSP:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select Game(Homebrew)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "ISO Files|*.iso|CSO Files|*.cso|All Files|*.*";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "Select Game";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Select Game Folder";
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(236, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Select Game(ISO/CSO)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.ForeColor = System.Drawing.Color.Green;
+            this.button3.Location = new System.Drawing.Point(12, 335);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(359, 93);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Install Game!!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(22, 271);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(79, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Refresh";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PSP_Game_Installer.Properties.Resources.nyan_cat;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(359, 195);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(533, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(30, 26);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "X";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Games:"});
+            this.listBox1.Location = new System.Drawing.Point(377, 44);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(186, 381);
+            this.listBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 440);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ PSP Game Installer/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace PSP_Game_Installer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                double freeSpace = drive.TotalFreeSpace;
+                double totalSpace = drive.TotalSize;
+                double percentFree = (freeSpace / totalSpace) * 100;
+                float num = (float)percentFree;
+
+                comboBox1.Items.Add(drive.Name);
+            }
+        }
+        string[] game;
+        string hom;
+        int homebrew = 0;
+        int iso = 0;
+        string gameiso = "";
+        int col = 0;
+        int coll = 0;
+        int colll = 0;
+        void showFolder()
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                iso = 0;
+                hom = folderBrowserDialog1.SelectedPath;         
+                homebrew = 1;
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Games:");
+                listBox1.Items.Add(hom);
+            }
+
+        }
+        void showIso()
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                iso = 1;
+                homebrew = 0;
+                game = openFileDialog1.FileNames;
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Games:");
+                foreach (string file in game)
+                {
+                    listBox1.Items.Add(file);
+                }
+            }
+
+            
+
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs, string psp)
+        {
+      
+                DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+                DirectoryInfo[] dirs = dir.GetDirectories();
+
+                if (!dir.Exists)
+                {
+                    throw new DirectoryNotFoundException(
+                        "Cant get dir: "
+                        + sourceDirName);
+                }
+
+                if (!Directory.Exists(destDirName))
+                {
+                    Directory.CreateDirectory(destDirName);
+                }
+
+                FileInfo[] files = dir.GetFiles();
+                foreach (FileInfo file in files)
+                {
+                    string temppath = Path.Combine(destDirName, file.Name);
+                    file.CopyTo(temppath, false);
+                }
+
+                if (copySubDirs)
+                {
+                    foreach (DirectoryInfo subdir in dirs)
+                    {
+                        string temppath = Path.Combine(destDirName, subdir.Name);
+                        DirectoryCopy(subdir.FullName, temppath, copySubDirs, psp);
+                    }
+                }
+
+                MessageBox.Show("Game Installed On PSP(" + psp + ")"); 
+          
+            
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            showFolder();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            showIso();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            if (listBox1.ItemHeight == 1)
+            {
+               
+            }
+            else
+            {
+                if (comboBox1.Text == "")
+                {
+
+                }
+                else
+                {
+                    Random k = new Random();
+                    col = k.Next(0, 255);
+                    coll = k.Next(0, 255);
+                    colll = k.Next(0, 255);
+
+                    button3.ForeColor = Color.FromArgb(col, coll, colll);
+                    timer1.Interval = k.Next(1, 1000);
+                }
+            }
+           
+        }
+        //
+        //source destination copy?Y/N
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string psp = comboBox1.Text;
+            if (comboBox1.Text == "")
+            {
+              MessageBox.Show("Error: No PSP Selected", "PSP Game Installer", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                return;
+            }
+            if (listBox1.ItemHeight == 1)
+            {
+                MessageBox.Show("Error: No Game Selected", "PSP Game Installer", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                return;
+            }
+            if (homebrew == 1) {
+                
+                string NameFolder = new DirectoryInfo(hom).Name;
+                string dest = psp+@"PSP\GAME\"+NameFolder;
+
+                DirectoryCopy(hom, dest, true, psp); }
+            if (iso == 1)
+            {
+                string dest = psp + @"ISO\";
+                try
+                {
+                    foreach (string file in game)
+                    {
+                        File.Copy(file, dest, true);
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Error: Something went wrong", "PSP Game Installer", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                double freeSpace = drive.TotalFreeSpace;
+                double totalSpace = drive.TotalSize;
+                double percentFree = (freeSpace / totalSpace) * 100;
+                float num = (float)percentFree;
+
+                comboBox1.Items.Add(drive.Name);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
